ce11a7e008c6cf8336119c9c8957ca0032274829
Dmitriy L <goldenpoint2000@gmail.com>
2021-05-17 17:04:50 +0300

    Added comments in set_environment.c

--- srcs/environment/set_environment.c
+++ srcs/environment/set_environment.c
@@ -6,20 +6,25 @@
 /*   By: eluceon <eluceon@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 09:25:30 by eluceon           #+#    #+#             */
-/*   Updated: 2021/05/17 16:24:12 by eluceon          ###   ########.fr       */
+/*   Updated: 2021/05/17 17:00:35 by eluceon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environment.h"
 
-t_env *set_environment(const char *envp[], t_env *env)
+/*
+** The set_environment() parses environment variables and values, the result is
+** stored in an array of strings.
+*/
+
+t_env	*set_environment(const char	*envp[], t_env *env)
 {
 	env->size = 0;
 	while (envp[env->size])
 		ft_strchr(envp[env->size++], '=');
 	if (!env->size)
 		return (NULL);
-	env->array  = duplicate_string_array(envp, env->size);
+	env->array = duplicate_string_array(envp, env->size);
 	if (!env->array)
 		return (NULL);
 	return (env);

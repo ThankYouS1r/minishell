765896275c3065115d67fd0cce54c0c0606352be
Etsuko Luceon <eluceon@ox-w1.msk.21-school.ru>
2021-05-19 13:53:11 +0300

    Modified

--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -6,7 +6,7 @@
 /*   By: eluceon <eluceon@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 08:58:31 by eluceon           #+#    #+#             */
-/*   Updated: 2021/05/19 12:44:51 by eluceon          ###   ########.fr       */
+/*   Updated: 2021/05/19 13:05:12 by eluceon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int	main(int argc, char *argv[], char *envp[])
 		return (1);
 //	set_signal_handlers(); // Will be done a litle bit later
 	set_environment((const char **)envp, &env);
+	printf("%s", read_line());
 	return (0); // Temp! Replace me!!!
 }
